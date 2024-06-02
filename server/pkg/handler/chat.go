056aacecb499bba25fc7d9c8ced114f70b030725
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-06-02 22:03:35 +0530

    Update chat.go to use io.ReadAll instead of ioutil.ReadAll

--- server/pkg/handler/chat.go
+++ server/pkg/handler/chat.go
@@ -2,7 +2,7 @@ package handler
 
 import (
     "encoding/json"
-    "io/ioutil"
+    "io"
     "net/http"
     "bytes"
 )
@@ -44,7 +44,7 @@ func generateResponse(prompt string) (string, error) {
         return "", err
     }
     defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+    body, err := io.ReadAll(resp.Body)
     if err != nil {
         return "", err
     }

984b7f49d19753930c63a2d6deb0e8a290748604
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-06-30 05:13:36 +0530

    Allow CORS
    Signed-off-by: Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>

--- server/pkg/middleware/logging.go
+++ server/pkg/middleware/logging.go
@@ -7,7 +7,15 @@ import (
 )
 
 func LoggingMiddleware(next http.Handler) http.Handler {
+	allowedHeaders := "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token"
+
 	return http.HandlerFunc(func(responseWriter http.ResponseWriter, response *http.Request) {
+		if origin := response.Header.Get("Origin"); origin != "" {
+			responseWriter.Header().Set("Access-Control-Allow-Origin", "*")
+			responseWriter.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			responseWriter.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+			responseWriter.Header().Set("Access-Control-Expose-Headers", "Authorization")
+		}
 		start := time.Now()
 		next.ServeHTTP(responseWriter, response)
 		log.Printf("method=%s path=%s duration=%s", response.Method, response.URL.Path, time.Since(start))

22fdc4217c237b8d024bc9a2427a1a529b966884
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-06-30 02:24:21 +0530

    Update bash script
    Signed-off-by: Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>

--- ollama/start.sh
+++ ollama/start.sh
@@ -7,21 +7,20 @@ ollama serve &
 sleep 5
 
 echo "Pulling llama3 model..." &&
-ollama pull llama3 &&
 
-echo "---------------------------------------------------" &&
-
-echo "Services Pull/Installed are as follow" &&
-ollama list &&
-
-echo "---------------------------------------------------" &&
+echo "###################################################" &&
 
 echo "Starting llama3 model..." &&
 ollama run llama3 &&
 
+echo "###################################################" &&
+
+echo "Services Pull/Installed are as follow" &&
+ollama list &&
+
 echo "llama3 Image Started..." &&
 
-echo "---------------------------------------------------" &&
+echo "###################################################" &&
 
 # Keep the script running
 tail -f /dev/null
